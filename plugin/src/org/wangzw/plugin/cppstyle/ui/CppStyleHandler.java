d275e1d0fa75bc0ba860f92014e36c5f168e7afe
Zhanwei Wang <zwang@pivotal.io>
2015-02-10 19:32:13 +0800

    enable dirty track, make save button gray after saving.

--- plugin/src/org/wangzw/plugin/cppstyle/ui/CppStyleHandler.java
+++ plugin/src/org/wangzw/plugin/cppstyle/ui/CppStyleHandler.java
@@ -1,19 +1,22 @@
 package org.wangzw.plugin.cppstyle.ui;
 
 import org.eclipse.cdt.ui.ICEditor;
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.EvaluationResult;
+import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.ISaveablePart;
+import org.eclipse.ui.ISaveablesSource;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.internal.util.Util;
+import org.eclipse.ui.internal.InternalHandlerUtil;
+import org.eclipse.ui.internal.SaveableHelper;
+import org.eclipse.ui.internal.WorkbenchPage;
+import org.eclipse.ui.internal.handlers.AbstractSaveHandler;
 import org.wangzw.plugin.cppstyle.CppCodeFormatter;
 
 /**
@@ -22,11 +25,12 @@ import org.wangzw.plugin.cppstyle.CppCodeFormatter;
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
-public class CppStyleHandler extends AbstractHandler {
+public class CppStyleHandler extends AbstractSaveHandler {
 	/**
 	 * The constructor.
 	 */
 	public CppStyleHandler() {
+		registerEnablement();
 	}
 
 	protected ICEditor getSaveableEditor(ExecutionEvent event) {
@@ -44,6 +48,7 @@ public class CppStyleHandler extends AbstractHandler {
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
+	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ICEditor editor = getSaveableEditor(event);
 
@@ -73,5 +78,37 @@ public class CppStyleHandler extends AbstractHandler {
 		}
 
 		return null;
+	}
+
+	@Override
+	protected EvaluationResult evaluate(IEvaluationContext context) {
+
+		IWorkbenchWindow window = InternalHandlerUtil
+				.getActiveWorkbenchWindow(context);
+		// no window? not active
+		if (window == null)
+			return EvaluationResult.FALSE;
+		WorkbenchPage page = (WorkbenchPage) window.getActivePage();
+
+		// no page? not active
+		if (page == null)
+			return EvaluationResult.FALSE;
+
+		// get saveable part
+		ISaveablePart saveablePart = getSaveablePart(context);
+		if (saveablePart == null)
+			return EvaluationResult.FALSE;
+
+		if (saveablePart instanceof ISaveablesSource) {
+			ISaveablesSource modelSource = (ISaveablesSource) saveablePart;
+			if (SaveableHelper.needsSave(modelSource))
+				return EvaluationResult.TRUE;
+			return EvaluationResult.FALSE;
+		}
+
+		if (saveablePart != null && saveablePart.isDirty())
+			return EvaluationResult.TRUE;
+
+		return EvaluationResult.FALSE;
 	}
 }
