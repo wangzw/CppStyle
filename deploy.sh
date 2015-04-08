6848744529d32e12fd8094383c8bc503ac044551
Zhanwei Wang <wangzw@wangzw.org>
2015-04-08 12:47:52 +0800

    deploy to different folder for different branch

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 BRANCH=gh-pages
 TARGET_REPO=wangzw/cppstyle
 OUTPUT_FOLDER=update/target/site
+SITE_DIR=$TRAVIS_BRANCH
 
 echo -e "Testing travis-encrypt"
 echo -e "$VARNAME"
@@ -16,10 +17,16 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd built_website
-    rm -rf update
-    mkdir -p update
-    cd update
+
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        SITE_DIR=update
+    fi
+
+    rm -rf $SITE_DIR
+    mkdir -p $SITE_DIR
+    cd $SITE_DIR
     rsync -rv --exclude=.git  ../../$OUTPUT_FOLDER/* .
+
     #add, commit and push files
     git add --all -f .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
