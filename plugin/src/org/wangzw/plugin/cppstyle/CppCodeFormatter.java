a5e1250ed5f17f5175845b91e859b94a70e21673
Zhanwei Wang <zwang@pivotal.io>
2015-05-10 10:29:02 +0800

    remove useless import

--- plugin/src/org/wangzw/plugin/cppstyle/CppCodeFormatter.java
+++ plugin/src/org/wangzw/plugin/cppstyle/CppCodeFormatter.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
