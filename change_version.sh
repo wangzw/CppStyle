c3b49cb217105fd6e4f44c34e40ca3e157066c1d
Zhanwei Wang <zwang@pivotal.io>
2015-06-17 10:36:24 +0800

    add script to change version

---
+++ change_version.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash 
+
+ROOT=`cd "\`dirname \"$0\"\`";pwd`
+
+OLD=$1
+NEW=$2
+
+[ -z "$OLD" ] && echo "current version is not specified" && exit 1
+[ -z "$NEW" ] && echo "target version is not specified" && exit 1
+
+echo "change version from $OLD to $NEW"
+
+case "`uname -s`" in
+    Darwin)
+        find $ROOT -name "*.xml" -or -name "*.MF" | xargs sed -i "" -e "s|$OLD|$NEW|"
+    ;;
+
+    *)
+        find $ROOT -name "*.xml" -or -name "*.MF" | xargs sed -i -e "s|$OLD|$NEW|"
+    ;;
+esac
+
+
+
+
+
+
+
+
+
+
+
+
